1f9498cc2341d3bb1aac0f2013a0e160a8d7c422
Blunde1 <lundeberent@gmail.com>
2018-03-30 11:32:19 +0200

    Fixed optimization error-handling routine

--- ExactSPA/src/includes/noncentral_chi_squared/optimization_nchisq.hpp
+++ ExactSPA/src/includes/noncentral_chi_squared/optimization_nchisq.hpp
@@ -9,20 +9,26 @@
 template<typename T>
 T sp_newton_opt(T s_start, T x, cgf_nchisq<T>& cgf, bool check_values=true, int maxniter=1000){
     int i;
-    T EPS = 3.0e-12, f_eps = 1.0e50, alpha = 1.0;
+    double alpha = 1.0;
+    T EPS = 3.0e-12, f_eps = 1.0e50;
     T g, h, s_old=s_start, s_new=s_start;
     for(i=0; i<maxniter; i++){
         g = cgf.deriv(s_old) - x;
         h = cgf.dderiv(s_old);
-        s_new -= alpha * g/h;
+        s_new = s_old - alpha * g/h;
         if(check_values){
-            if(abs(s_new - s_old) <= EPS){
+            if(isnan(cgf(s_new))){
+                alpha = 0.5*alpha;
+                s_old = s_start;
+                i=0;
+            }
+            else if(abs(s_new - s_old) <= EPS){
                 break;
             } else if(cgf(s_new) <= (cgf(s_old) + f_eps) ){
                 s_old = s_new;
             } else{
                 alpha = 0.5*alpha;
-                //s_old = s_start;
+                s_old = s_start;
                 i=0;
             }
         } else{
